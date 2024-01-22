56d61559ebcff3da2af7ee3d71385665c70c655c
Travisken <123268134+Travisken@users.noreply.github.com>
2024-01-22 13:48:10 +0100

    force push

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tabs',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
 export class TabsPage {
 
   constructor() {}
+
+ 
+
+  
+
 
 }
